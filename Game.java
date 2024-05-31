04950b1e6772a339024b147b882f83426d9224e7
bradley121901 <bradley121901@gmail.com>
2024-05-31 17:59:30 -0400

    initial files wip

---
+++ Game.java
@@ -0,0 +1,10 @@
+public class Game {
+    private int wins;
+    private int losses;
+    //default constructor
+    public Game(){
+        this.wins = 0;
+        this.losses = 0;
+    }
+    
+}
---
+++ Suit.java
@@ -0,0 +1,7 @@
+public enum Suit {
+    DIAMOND("Diamonds"), CLUB("Clubs"), HEART("Hearts"), SPADE("Spades");
+    String suit;
+    Suit(String suit){
+        this.suit = suit;
+    }
+}
